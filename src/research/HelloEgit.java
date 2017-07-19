f0ab0a320dd1786e248b6c1171249219178c2802
liupeng_17 <liupeng120226@163.com>
2017-07-19 14:28:27 +0800

    second modify

--- src/research/HelloEgit.java
+++ src/research/HelloEgit.java
@@ -4,5 +4,6 @@ public class HelloEgit {
 	public static void main(String[] args){
 		System.out.println("hello Egit");
 		System.out.println("first modify");
+		System.out.println("second modify");
 	}
 }
